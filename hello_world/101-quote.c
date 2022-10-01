7c16491b3f04754401d2e2d093be4332af59c7d3
s4iTen <ychouk102@gmail.com>
2022-10-01 01:40:33 -0700

    display without printf and puts

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-
-int write(int filedes, const char *buf, unsigned int nbyte);
-int main( int argc, char** argv )
+#include <unistd.h>
+int main(void)
 {
   write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 13);
 
