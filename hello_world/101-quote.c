150efbbf29803a1092ee68631b95f675e4594489
s4iTen <ychouk102@gmail.com>
2022-10-01 00:45:03 -0700

    display with out printf or puts

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-const char msg[] = "and that piece of art is useful\" '\'- Dora Korpar, 2015\-10\-19\n";
-syscall(SYS_write, 1, msg, sizeof(msg));
-return 0;
+  system("echo Hello world!");
+  return 0;
 } 
