d51c93801041e94ff5d29b53d1678f2c6e76e570
s4iTen <ychouk102@gmail.com>
2022-10-01 00:57:12 -0700

    display output without printf or puts

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-int main(void)
+
+int write(int filedes, const char *buf, unsigned int nbyte);
+int main( int argc, char** argv )
 {
-  system("echo Hello world!");
+  write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 13);
+
   return 0;
 } 
