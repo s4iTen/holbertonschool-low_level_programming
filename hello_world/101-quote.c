16a7325a5a0a407b8979f6edc400472605054319
s4iTen <ychouk102@gmail.com>
2022-10-01 01:47:20 -0700

    display without printf and puts

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+/**
+ *
+ *main - display output without using print or puts command
+ *
+ *Return: Always 1 (Success)
+ *
+ */
 int main(void)
 {
-  write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-  return (1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 } 
