aec07e936499e89e877d58805e5cff8464afe39a
s4iTen <ychouk102@gmail.com>
2022-10-03 02:37:07 -0700

    rand num assigned to a variable

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,24 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+/**
+ * more headers goes there
+ */
 
-void main(int)
+/**
+ * betty style doc for function main goes there */
+int main(void)
 {
-  printf (rand());
-  getch();
-}  
+  int n;
+
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+  if (n > 0)
+    {
+      printf("%d\n ,is positive ",n )
+    }
+  else if (n < 0)
+    {
+      printf("%d\n ,is negative ",n )
+    }
+  return (0);
+}
