a229510a3e9f7cac7a5d348be24b594c8e15f591
s4iTen <ychouk102@gmail.com>
2022-10-03 02:52:28 -0700

    rand num assigned to a variable

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,11 +15,11 @@ int main(void)
   n = rand() - RAND_MAX / 2;
   if (n > 0)
     {
-      printf("\n"n ,"is positive");
+      printf(n "is positive ");
     }
   else if (n < 0)
     {
-      printf("\n"n ,"is negative");
+      printf(n ,"is negative");
     }
   return (0);
 }
