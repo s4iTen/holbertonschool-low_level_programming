2f70577b37bca3a9d80e13491bb8b9761d91ff8a
s4iTen <ychouk102@gmail.com>
2022-10-03 13:30:29 -0700

    putchar

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char ab;
+for (ab = 97; ab < 123; ab++)
+{
+putchar(ab);
+}
+putchar('\n');
+return (0);
+}
