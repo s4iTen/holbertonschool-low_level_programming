cfde7eb056c3ef8299efbc2e7b169ba93f02fdc8
s4iTen <ychouk102@gmail.com>
2022-10-03 12:16:04 -0700

    print the last digit and compare it

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,10 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
+ * main -Entry point for c program
  *
- *main - Entry point for c program
- *
- *Return: Always 0 (Success)
+ *Return: Always 0
  */
 int main(void)
 {
