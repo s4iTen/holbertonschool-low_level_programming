14bbd88aa6cb0e1a9d4b459d72ee22e2686a4a87
s4iTen <ychouk102@gmail.com>
2022-10-03 03:24:03 -0700

    rand num

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -6,12 +6,12 @@
  */
 
 /**
- * betty style doc for function main goes there 
+ *betty style doc for function main goes there 
  */
 int main(void)
 {
 int n;
-  
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
