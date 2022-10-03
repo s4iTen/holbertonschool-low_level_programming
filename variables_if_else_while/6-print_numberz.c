7a40e2da32cdc44365420c86bcb0a0565b856eb8
s4iTen <ychouk102@gmail.com>
2022-10-03 13:54:12 -0700

    putchar without char variable

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int ab;
+for (ab = 48; ab < 58; ab++)
+{
+putchar(ab);
+}
+putchar('\n');
+return (0);
+}
