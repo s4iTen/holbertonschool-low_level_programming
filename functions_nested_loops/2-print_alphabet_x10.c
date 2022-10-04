bd4bfcfad9fa606676195ebb60f4870f3db92f8c
s4iTen <ychouk102@gmail.com>
2022-10-04 13:51:22 -0700

    putchar *10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,22 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_alphabet - Prints the alphabet in lowercase.
+ * print_alphabet_x10 - Prints the alphabet in lowercase.
  *
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
 {
-  char i;
+char i;
 
-  int b;
+int b;
 
-  for (b = 0; b < 10; b++)
-    {
-  for (i = 97; i <= 122; i++)
-    {
-      _putchar(i);
-    }
-  _putchar('\n');
-    }
+for (b = 0; b < 10; b++)
+{
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
 }
