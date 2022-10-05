06d4c7361479c61b77013704f0cf7d3450cdaa45
s4iTen <ychouk102@gmail.com>
2022-10-05 17:33:02 +0200

    print last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _abs - check the code
+ * print_last_digit - check the code
  * @n: in the integer
  * Return: change the return if
  */
 int print_last_digit(int n)
 {
-	_putchar(( n % 10 ) + '0');
-	return (0);
+	_putchar((n % 10) + '0');
+	return (n % 10);
 }
