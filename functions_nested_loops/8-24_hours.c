a056e75876592d795050743715393cff68c839d3
s4iTen <ychouk102@gmail.com>
2022-10-05 23:14:04 +0200

    colock

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,23 +9,24 @@ void jack_bauer(void)
 	int hour, minute;
 
 	hour=minute=0;
-	while (1)
+	for (minute = 0; minute <= 60; minute++)
 	{
+		
 		
 		printf("%02d : %02d\n", hour, minute);
 		fflush(stdout);
-		minute++;
 		if(minute==60)
 		{
             hour+=1;
             minute=0;
         }
-        if(hour==23)
+		if(hour == 23 && minute == 59)
 		{
-            minute=59;
-			continue;
-        }
+			break;
+		}
+        
 		sleep(0.1);
+		
 	}
 	
 }
