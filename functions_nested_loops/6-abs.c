3afdf4ae0f23684d79e829b1d79ab195d23076db
s4iTen <ychouk102@gmail.com>
2022-10-05 16:44:27 +0200

    only positive num

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,14 +1,16 @@
 #include "main.h"
-
-
-
+/**
+ * _abs - check the code
+ *
+ * Return: change the return if
+ */
 int _abs(int n)
 {
 
-	if ( n >= 0)
+	if (n >= 0)
 	{
-		return(n);
+		return (n);
 	}
 	else
-		return(-n);
+		return (-n);
 }
