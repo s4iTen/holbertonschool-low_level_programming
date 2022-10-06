2906817b24589cc35ab6497dd24b6a6185394ec5
s4iTen <ychouk102@gmail.com>
2022-10-06 21:32:38 +0200

    multiple two int

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Description: 'is upper'
+ *
+ * Return: always 0 or 1
+ * @a: - integer to use
+ * @b: - integer to use
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
