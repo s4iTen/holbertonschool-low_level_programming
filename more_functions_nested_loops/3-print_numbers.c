ad45e4d62bddbae88c72853fb86a1d7b2934f74c
s4iTen <ychouk102@gmail.com>
2022-10-06 22:02:23 +0200

    print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - check the code
+ * @i: this is an intiger
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
