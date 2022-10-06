6f2d1f646109691753e0146cbc4565ee85d99ff9
s4iTen <ychouk102@gmail.com>
2022-10-06 13:56:01 +0200

    add

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,16 +1,14 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
+ * add - Description: 'adding two numbers'
  *
- * Return: Always 0.
+ * @a:  - integer to use
+ *
+ * @b: - integer to use
+ *
+ * Return: a + b
  */
-int main(void)
+int add(int a, int b)
 {
-    int n;
-
-    n = add(89, 9);
-    printf("%d\n", n);
-    return (0);
+	return (a + b);
 }
