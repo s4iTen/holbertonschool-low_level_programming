1afd9d39563a75c1793b6dbf8d2adc1e18e89cbc
s4iTen <ychouk102@gmail.com>
2022-10-06 14:03:47 +0200

    98 Battery Street, the OG

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,32 +2,32 @@
 #include <stdio.h>
 void print_to_98(int n)
 {
-    int i;
-    if ( n < 98)
+	int i;
+	if (n < 98)
 	{
-		for (i = n ;i <= 98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
 			{
-				printf("%d, ",i);
+				printf("%d, ", i);
 			}
 			else
 			{
-				printf("%d\n",i);
+				printf("%d\n", i);
 			}
 		}
 	}
-    else 
+	else
 	{
-		for (i = n;i >=98; i--)
+		for (i = n; i >= 98; i--)
 		{
 			if (i != 98)
 			{
-				printf("%d, ",i);
+				printf("%d, ", i);
 			}
 			else
 			{
-				printf("%d\n",i);
+				printf("%d\n", i);
 			}
 		}
 	}
