0f04da34989bc3f6e71ee708047d11c652221638
s4iTen <ychouk102@gmail.com>
2022-10-07 19:34:51 +0200

    function that print all numbers from 1 to 100 and underplaces all multiplies of  3 and 5

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,21 +15,21 @@ int main(void)
 
 		if (((i % 3) == 0) && ((i % 5) == 0))
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else if ((i % 3) == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 		else if ((i % 5) == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		else if (i != 100)
 			printf("%d ", i);
 		else
-			printf("%d ", i);
+			printf("%d", i);
 	}
-	putchar('\n');
+	printf("\n");
 	return (0);
 }
