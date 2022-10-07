9949311c7b20936afc8a60f347366491cb3d3ef1
s4iTen <ychouk102@gmail.com>
2022-10-07 13:08:53 +0200

    times tables

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -11,25 +11,21 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
+		_putchar('0');
 
-		for (b = 0; b <= 9; b++)
+		for (b = 1; b <= 9; b++)
 		{
-			c = a * b;
-			if ((c / 10) != 0)
-			{
-				_putchar((c / 10) + '0');
-			}
-			_putchar((c % 10) + '0');
-			if (b != 9)
-			{
-				_putchar(',');
-			}
+			_putchar(',');
 			_putchar(' ');
-			if ((c / 10) == 0)
-			{
+
+			c = a * b;
+
+			if (c <= 9)
 				_putchar(' ');
-			}
-			
+			else
+				_putchar((c / 10) + '0');
+
+			_putchar((c % 10) + '0');
 		}
 		_putchar('\n');
 	}
