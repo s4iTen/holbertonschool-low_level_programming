855d52fe4bc4095d574aae239a1f85a782fa3f3b
s4iTen <ychouk102@gmail.com>
2022-10-07 19:55:32 +0200

    function that print all numbers from 1 to 100 and underplaces all multiplies of  3 and 5

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,35 +1,36 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - function that checks for uppercase character.
- * @i: the int for the paramaters of my function
- * @count: the int for the paramaters of my function
+ * main - Description: 'underplace the 3 and the 5'
+ * Return: Always 0
+ *
  */
 int main(void)
 {
 	int i, count;
 
 	count = 100;
+	i = 1;
 	for (i = 1; i <= count; i++)
 	{
-			if (((i % 3) == 0) && ((i % 5) == 0))
-			{
-				printf("FizzBuzz");
-			}
-			else if ((i % 3) == 0)
-			{
-				printf("Fizz");
-			}
-			else if ((i % 5) == 0)
-			{
-				printf("Buzz");
-			}
-			else 
-				printf("%d", i);
-			if (i != 100)
-				printf(" ");
-			else
-				printf("\n");
+		if (((i % 3) == 0) && ((i % 5) == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+		else
+			printf("\n");
 	}
 	return (0);
 }
