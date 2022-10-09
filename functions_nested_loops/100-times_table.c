93a8cbd9191539294840995cc519b77417883bb8
s4iTen <ychouk102@gmail.com>
2022-10-09 18:24:22 +0200

    time table

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * print_times_table - Description: 'print time table'
+ *
+ * @n: - integer to use
+ */
+void print_times_table(int n)
+{
+	int a;
+	int b;
+	int c;
+
+    if(n>=0 && n<=15)
+    {
+		for (a = 0; a <= n; a++)
+		{
+			_putchar('0');
+
+			for (b = 1; b <= n; b++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				c = a * b;
+				
+				if (c <= 9)
+					_putchar(' ');
+				if (c <= 99)
+					_putchar(' ');
+				if (c >= 100)
+				{
+					_putchar((c / 100) + '0');
+					_putchar((c / 10) % 10 + '0');
+				}
+				else if (c <= 99 && c > 9)
+				{
+					_putchar((c / 10) + '0');
+				}
+					
+
+				_putchar((c % 10) + '0');
+			}
+			_putchar('\n');
+		}
+    }
+}
