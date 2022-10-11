76c3bae5c8ad7fad80523652ac6da6526a8d39c6
s4iTen <ychouk102@gmail.com>
2022-10-11 12:03:17 +0200

    print rev string

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_rev - Description 'Display reverse string'.
+ * @s: - it is a pinter called str
+ *
+ */
+void print_rev(char *s)
+{
+	int i;
+	int len = strlen(s);
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
