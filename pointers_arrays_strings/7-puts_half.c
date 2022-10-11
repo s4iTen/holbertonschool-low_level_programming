2e391a21d0a17a1624d616be28a80f773f48ce0c
s4iTen <ychouk102@gmail.com>
2022-10-11 21:18:45 +0200

    print the half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -11,14 +11,14 @@ void puts_half(char *str)
 
 	if ((len % 2) == 0)
 	{
-		for (i = (len / 2); i <= len; i++)
+		for (i = (len / 2); i < len; i++)
 		{
 			_putchar(str[i]);
 		}
 	}
 	else
 	{
-		for (i = ((len - 1) / 2); i < len - 1; i++)
+		for (i = ((len - 1) / 2); i <= len; i++)
 		{
 			_putchar(str[i]);
 		}
