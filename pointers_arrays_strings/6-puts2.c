70d8bed4509c2dfe34fdb37c199c7f1316d47490
s4iTen <ychouk102@gmail.com>
2022-10-11 20:37:30 +0200

    print pair

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,12 +7,12 @@
  */
 void puts2(char *str)
 {
-    int len = strlen(str);
-    int i;
-    
-    for (i = 0; i < len; i += 2)
-    {
-        _putchar(str[i]);
-    }
-    _putchar('\n');
+	int len = strlen(str);
+	int i;
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
