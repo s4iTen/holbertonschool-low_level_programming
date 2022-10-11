4e194edee283ce791b402476317e484aaab3d06f
s4iTen <ychouk102@gmail.com>
2022-10-11 21:08:18 +0200

    main head

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <string.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -6,3 +8,4 @@ int _putchar();
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
