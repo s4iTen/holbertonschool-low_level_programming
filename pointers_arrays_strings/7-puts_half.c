40568ef69cdedcd6b6332e26e221fabe8a14102b
s4iTen <ychouk102@gmail.com>
2022-10-11 21:07:18 +0200

    print the half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts_half - Description 'print the last half'.
+ * @str: - it is a pinter called str
+ *
+ */
+void puts_half(char *str)
+{
+	int i;
+	int len = strlen(str);
+
+	if ((len % 2) == 0)
+	{
+		for (i = (len / 2); i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = ((len - 1) / 2); i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
