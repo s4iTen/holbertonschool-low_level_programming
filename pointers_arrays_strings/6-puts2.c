4a7ccaff383ec6ea359c4166184736aee0fc6e05
s4iTen <ychouk102@gmail.com>
2022-10-11 20:34:42 +0200

    print pair

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - Description 'print pair'.
+ * @str: - it is a pinter called str
+ *
+ */
+void puts2(char *str)
+{
+    int len = strlen(str);
+    int i;
+    
+    for (i = 0; i < len - 1; i += 2)
+    {
+        _putchar(str[i]);
+    }
+    _putchar('\n');
+}
