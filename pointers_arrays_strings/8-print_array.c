cf2d2a2472429e497d73d3b910ccf98f3fa8011a
s4iTen <ychouk102@gmail.com>
2022-10-12 11:11:39 +0200

    print array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - Description 'print array'.
+ * @n: - it is an intiger called n
+ * @a: - it is a pinter called *a
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != (n - 1))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
