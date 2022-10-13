833437e754e1dbde97ed9172415805adaa5da6ea
s4iTen <ychouk102@gmail.com>
2022-10-13 23:37:46 +0200

    comparisation

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -13,14 +13,18 @@ int _strcmp(char *s1, char *s2)
 
 	while (*(s1 + i) != '\0' && *(s2 + j) != '\0')
 	{
-		i++;
-		j++;
-	}
 		if (*s1 == *s2)
 		{
 			return (0);
+			i++;
+			j++;
 		}
 		else
+		{
 			return (*s1 - *s2);
-	return (1);
+			i++;
+			j++;
+		}
+	}
+	return (*s1 - *s2);
 }
