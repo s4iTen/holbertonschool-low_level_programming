f13a3cb1e4002d29a60e49cc90bdf6bf625018e9
s4iTen <ychouk102@gmail.com>
2022-10-13 23:24:32 +0200

    comparisation

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcmp - Description 'coppy arrays'.
+ * @s1: - it is a pointer called dest
+ * @s2: - it is a pointer called src
+ *
+ * Return: - Always dest as return
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int j = 0;
+
+	while (*(s1 + i) != '\0' && *(s2 + j) != '\0')
+	{
+		if (*s1 == *s2)
+		{
+			return (0);
+		}
+		else
+		{
+			return (*s1 - *s2);
+		}
+		i++;
+		j++;
+	}
+	return (*s1 - *s2);
+}
