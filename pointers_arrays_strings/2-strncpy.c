ce7828e0ea3cb666ac83e06f64d6dab0ffc858fb
s4iTen <ychouk102@gmail.com>
2022-10-13 11:17:32 +0200

    coppy string pointer

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *_strncat - Description 'coppy arrays'.
+ * @dest: - it is a pointer called dest
+ * @src: - it is a pointer called src
+ * @n: - it is an integer called n
+ * Return: - Always dest as return
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (*(dest + j) != '\0' && i < n)
+	{
+		*(dest + j) = *(src + i);
+		i++;
+		j++;
+	}
+	while (i > n)
+	{
+		*(dest + j) = '\0';
+	}
+	return (dest);
+}
