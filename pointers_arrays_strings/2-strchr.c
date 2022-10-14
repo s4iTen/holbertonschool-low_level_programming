d901a5ddb9db34c4d4ad9bee38781a1351a568b8
s4iTen <ychouk102@gmail.com>
2022-10-15 01:15:45 +0200

    locate a char in str

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,18 +1,22 @@
 #include "main.h"
-
+/**
+ * *_strchr - Description 'count lenght of pointer'.
+ * @s: - it is a pinter called s
+ * @c: - it is an character called c
+ * Return: - Always s or null
+ */
 char *_strchr(char *s, char c)
 {
-    while (*s != '\0')
-    {
-        if(*s == c)
-            return (s);
-        s = s + 1;
-    }
-    if (*s == c)
-    {
-        return (s);
-    }
-    else
-        return (NULL);
-    
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s = s + 1;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	else
+		return (NULL);
 }
