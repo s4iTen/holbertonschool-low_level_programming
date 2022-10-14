de1d65c3374a61f1410f7caf7a69237b0bc1bfdc
s4iTen <ychouk102@gmail.com>
2022-10-14 19:21:45 +0200

    copy from pointer to pointer

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
- * *_memcpy - Description 'coppy arrays'.
- * @a: - it is a pointer called dest
- * Return: - Always 'a'
+ * *cap_string - Description 'coppy arrays'.
+ * @dest: - it is a pointer called dest
+ * @src: - it is a pointer called src
+ * @n: - it is an integer called n
+ * Return: - Always 's'
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
