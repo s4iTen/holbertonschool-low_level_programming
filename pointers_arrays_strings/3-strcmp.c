919f48ec71804150ee40de55d0b1edb5a9e4dd21
s4iTen <ychouk102@gmail.com>
2022-10-14 11:25:10 +0200

    comaprisation

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,19 +9,14 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int j = 0;
 
-	while (*(s1 + i) != '\0' && *(s2 + j) != '\0')
+	while (*(s1 + i) != '\0' && *(s2 + i) != '\0')
 	{
-		
-		if (*s1 < *s2 || *s1 > *s2)
+		if (*(s1 + i) != *(s2 + i))
 		{
-			return (*s1 - *s2);
-			i++;
-			j++;
-		}
-		else
-			return (0);
+			return (*(s1 + i) - *(s2 + i));
+				}
+		i++;
 	}
 	return (0);
 }
