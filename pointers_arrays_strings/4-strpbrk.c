9b6826af682c874cd79e539314a337741a928203
s4iTen <ychouk102@gmail.com>
2022-10-15 18:37:38 +0200

    search a string

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strspn - Description 'Display reverse string'.
+ * @s: - it is a pinter called s
+ * @accept: - it is a pinter called accept
+ * Return: - Always return i
+ */
+char *_strpbrk(char *s, char *accept)
+{
+    int i = 0;
+    int j = 0;
+
+    while (*(s + i) != '\0')
+    {
+        i++;
+        if (*(accept) + j == *(s + i))
+        {
+            
+            return (accept);
+        }
+    }
+    return (NULL);
+}
