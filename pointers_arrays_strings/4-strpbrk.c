071b02adddf86761e6f48ab3cf5aa69076e0be32
s4iTen <ychouk102@gmail.com>
2022-10-16 02:16:34 +0200

    return

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,24 +1,34 @@
 #include "main.h"
 /**
- * _strspn - Description 'Display reverse string'.
+ * *_strpbrk - Description 'Display reverse string'.
  * @s: - it is a pinter called s
  * @accept: - it is a pinter called accept
- * Return: - Always return i
+ * Return: - Always return 's + i' or 'NULL'
  */
 char *_strpbrk(char *s, char *accept)
 {
-    int i = 0;
-    int j = 0;
+	int i = 0;
+	int j;
+	int k = 0;
 
-    while (*(s + i) != '\0')
-    {
-        if (*(accept) + j == *(s + i))
-        {
-            return (accept);
-        }
-        i++;
-        j++;
-    }
-    
-    return (NULL);
+	while (*(s + i) != '\0')
+	{
+		j = 0;
+		while (*(s + i) != *(accept + j) && *(accept + j) != '\0')
+		{
+			j++;
+		}
+
+		if (*(accept + j) != '\0')
+		{
+			k = 1;
+			break;
+		}
+		i++;
+	}
+
+	if (k == 1)
+		return ((s + i));
+	else
+		return (NULL);
 }
