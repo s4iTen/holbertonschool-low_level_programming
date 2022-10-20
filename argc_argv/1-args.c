d12ff67647e288f1267ca0f1745af4e7e42f7be2
s4iTen <ychouk102@gmail.com>
2022-10-20 12:20:26 +0200

    count the arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - function that print the number of arguments
+ * @argc: - counter of argv
+ * @argv: - its a pointer of array
+ * Return: - return always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i != argc)
+	{
+		i++;
+		argv++;
+	}
+	printf("%d\n", i);
+	return (0);
+}
