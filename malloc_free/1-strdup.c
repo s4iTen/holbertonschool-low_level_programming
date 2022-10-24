4466ccd93afe646957fc39d3e2e3c6eb4f45c4d7
s4iTen <ychouk102@gmail.com>
2022-10-24 12:50:15 +0200

    duplicate

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,26 +1,30 @@
 #include "main.h"
-
+/**
+ * *_strdup - prints buffer in hexa
+ * @str: it is a pointer called 'str'
+ * Return: Pointer or null
+ */
 char *_strdup(char *str)
 {
-    int len = strlen(str);
-    int i = 0;
-    int j = 0;
-    char *p;
+	int len = strlen(str);
+	int i = 0;
+	int j = 0;
+	char *p;
 
-    if (str == NULL)
+	if (str == NULL)
 	{
 		return (NULL);
 	}
-    p = malloc(len * sizeof(char));
-    if (p == NULL)
-    {
-        return (NULL);
-    }
-    while (j < len)
-    {
-        p[j] = str[i];
-        j++;
-        i++;
-    }
-    return (p);
+	p = malloc(len * sizeof(char));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	while (j < len)
+	{
+		p[j] = str[i];
+		j++;
+		i++;
+	}
+	return (p);
 }
