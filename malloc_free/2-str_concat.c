eae5bb5f2d5aac2b0ca69084e568c2102f6db3cc
s4iTen <ychouk102@gmail.com>
2022-10-24 16:58:21 +0200

    concatination

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,48 +1,53 @@
 #include "main.h"
-
+/**
+ * *str_concat - prints buffer in hexa
+ * @s2: it is a pointer called 'str'
+ * @s1: it is a pointer called 'str'
+ * Return: Pointer or null
+ */
 char *str_concat(char *s1, char *s2)
 {
-    int i = 0;
-    int j = 0;
-    int k;
-    int l = 0;
-    int len1 = 0;
-    int len2 = 0;
-    char *p;
+	int i = 0;
+	int j = 0;
+	int k;
+	int l = 0;
+	int len1 = 0;
+	int len2 = 0;
+	char *p;
 
-    for (i = 0; s1[i]; i++)
-    {
-        len1++;
-    }
-    for (j = 0; s2[j] ; j++)
-    {
-        len2++;
-    }
-    k = len1 + len2;
-    if (s1 == NULL)
-    {
-        return (NULL);
-    }
-    if (s2 == NULL)
-    {
-        return (NULL);
-    }
-    i = 0;
-    p = malloc((k + 1)* sizeof(char));
-    if (p == NULL)
-    {
-        return (NULL);
-    }
-    while (l < len1)
-    {
-        p[l] = s1[l];
-        l++;
-    }
-    while (i <= len2)
-    {
-        p[l] = s2[i];
-        l++;
-        i++;
-    }
-    return (p);
+	for (i = 0; s1[i]; i++)
+	{
+		len1++;
+	}
+	for (j = 0; s2[j]; j++)
+	{
+		len2++;
+	}
+	k = len1 + len2;
+	if (s1 == NULL)
+	{
+		return (NULL);
+	}
+	if (s2 == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	p = malloc((k + 1) * sizeof(char));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	while (l < len1)
+	{
+		p[l] = s1[l];
+		l++;
+	}
+	while (i <= len2)
+	{
+		p[l] = s2[i];
+		l++;
+		i++;
+	}
+	return (p);
 }
