f5c7a20ce287eacdbc4c4b2b0718857bdf7d5943
s4iTen <ychouk102@gmail.com>
2022-10-24 16:59:57 +0200

    concatination

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,22 +16,14 @@ char *str_concat(char *s1, char *s2)
 	char *p;
 
 	for (i = 0; s1[i]; i++)
-	{
 		len1++;
-	}
 	for (j = 0; s2[j]; j++)
-	{
 		len2++;
-	}
 	k = len1 + len2;
 	if (s1 == NULL)
-	{
 		return (NULL);
-	}
 	if (s2 == NULL)
-	{
 		return (NULL);
-	}
 	i = 0;
 	p = malloc((k + 1) * sizeof(char));
 	if (p == NULL)
