561aaffe851f06c1be97ed27a4ad4eb884b2bf1c
s4iTen <ychouk102@gmail.com>
2022-10-25 14:15:59 +0200

    allocate

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,15 @@
 #include "main.h"
-
+/**
+ * *malloc_checked - check the code
+ * @b: it is a variable called 'b'
+ * Return: Always pointer.
+ */
 void *malloc_checked(unsigned int b)
 {
-    unsigned int *p;
+	unsigned int *p;
 
-    p = malloc(b);
-    if (p == NULL)
-        exit(98);
-    return (p);
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
 }
