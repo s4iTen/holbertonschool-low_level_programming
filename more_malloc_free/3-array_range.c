55a3b71dbbd685a19416f80a3d00150dea9715c9
s4iTen <ychouk102@gmail.com>
2022-10-26 20:42:43 +0200

    create array of int

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,21 +1,26 @@
 #include "main.h"
-
+/**
+ * array_range - check the code
+ * @min: it is a variable called 'min'
+ * @max: it is a variable called 'max'
+ * Return: Always pointer or NULL
+ */
 int *array_range(int min, int max)
 {
-    int *p;
-    int i;
-    int j = 0;
-    int k = max - min;
+	int *p;
+	int i;
+	int j = 0;
+	int k = max - min;
 
-    if (min > max)
-        return (NULL);
-    p = malloc((k + 1) * sizeof(int));
-    if (p == NULL)
-        return (NULL);
-    for (i = min; i < (max + 1); i++)
-    {
-        p[j] = i;
-        j++;
-    }
-    return (p);
+	if (min > max)
+		return (NULL);
+	p = malloc((k + 1) * sizeof(int));
+	if (p == NULL)
+		return (NULL);
+	for (i = min; i < (max + 1); i++)
+	{
+		p[j] = i;
+		j++;
+	}
+	return (p);
 }
