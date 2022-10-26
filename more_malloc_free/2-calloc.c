71e3ba1fb45370757ab6ab88bfbbff886e6f3e56
s4iTen <ychouk102@gmail.com>
2022-10-26 15:26:31 +0200

    calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,24 +1,28 @@
 #include "main.h"
 /**
  * _calloc - check the code
- * @nmemb: it is a variable called 'nmemb'
- * @size: it is a variable called 'size'
+ * @nmemb: it is a pointer called 'nmemb'
+ * @size: it is a pointer called 'size'
  * Return: Always pointer or NULL
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *p;
+	int *p;
 	int i = 0;
 
 	if (nmemb == 0 || size == 0)
+	{
 		return (NULL);
-
-	p = (char *)malloc(nmemb * size);
+	}
+	p = malloc(nmemb * size);
 	if (p == NULL)
+	{
 		return (NULL);
-
-	for (i = 0; p[i]; i++)
+	}
+	while (p[i])
+	{
 		p[i] = 0;
-
+		i++;
+	}
 	return (p);
 }
