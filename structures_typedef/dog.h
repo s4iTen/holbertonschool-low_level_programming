4333e9967131d252e0e142a438c4bb3ba2ce5a59
s4iTen <ychouk102@gmail.com>
2022-10-31 11:34:11 +0100

    header file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,8 @@
+#ifndef HEADERFILE_H
+#define HEADERFILE_H
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#ifndef HEADERFILE_H
-#define HEADERFILE_H
 /**
  * struct dog - check the code
  * @name: its an array of type char called 'name'
@@ -15,5 +15,5 @@ struct dog
 	float age;
 	char *owner;
 };
-#endif
 void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
