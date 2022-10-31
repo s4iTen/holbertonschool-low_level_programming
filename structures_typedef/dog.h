ae73148f291c6d269db1722de09ef0b18ce255f9
s4iTen <ychouk102@gmail.com>
2022-10-31 10:42:21 +0100

    struct of dog id, the age and the owner

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,3 +1,9 @@
+/**
+ * struct dog - check the code
+ * @name: its an array of type char called 'name'
+ * @age: its an variable of type float called 'age'
+ * @owner: its an array of type char called 'owner'
+ */
 struct dog
 {
 	char *name;
