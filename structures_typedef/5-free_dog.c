64f291019a1a89522722be4aad5294beb8d6ad6c
s4iTen <ychouk102@gmail.com>
2022-11-02 11:25:58 +0100

    free struct

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,12 +1,15 @@
 #include <stdlib.h>
 #include "dog.h"
-
+/**
+ * free_dog - it is a function that free all the memory that the struct taked
+ * @d: it is a pointer called "d"
+ */
 void free_dog(dog_t *d)
 {
-    if (d != NULL)
-    {
-        free(d->name);
-        free(d->owner);
-        free(d);
-    }
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
