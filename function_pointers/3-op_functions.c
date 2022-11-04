18654b4cf349aa5ee144b85f4c10193361ece733
s4iTen <ychouk102@gmail.com>
2022-11-04 11:06:41 +0100

    first file

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - function add two integers
+ * @a: it is an integer called 'a'
+ * @b: it is an integer called 'b'
+ * Return: Always integer
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - function add two integers
+ * @a: it is an integer called 'a'
+ * @b: it is an integer called 'b'
+ * Return: Always integer
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function add two integers
+ * @a: it is an integer called 'a'
+ * @b: it is an integer called 'b'
+ * Return: Always integer
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - function add two integers
+ * @a: it is an integer called 'a'
+ * @b: it is an integer called 'b'
+ * Return: Always integer
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - function add two integers
+ * @a: it is an integer called 'a'
+ * @b: it is an integer called 'b'
+ * Return: Always integer
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
