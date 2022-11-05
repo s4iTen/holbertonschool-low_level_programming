8f2d7b2dbecc62c8a20d6e4c50b10f7683ea81fd
s4iTen <ychouk102@gmail.com>
2022-11-05 19:23:20 +0100

    print the strings followed by new line

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,17 +1,23 @@
 #include "variadic_functions.h"
-
+/**
+ * print_strings - finction that print the strings with new line
+ * @separator: it is a pointer that have the separator between the numbers
+ * @n: it is a variable that its the number of the arguments
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-    va_list list;
+	va_list list;
 	unsigned int i = 0;
+	char *p;
 
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
-        if ((va_arg(list, char*)) != NULL)
-		    printf("%s", va_arg(list, char*));
-        else
-            printf("(nil)");
+		p = (va_arg(list, char *));
+		if (p == NULL)
+			printf("(nil)");
+		else
+			printf("%s", p);
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
