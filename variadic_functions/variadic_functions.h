921383ac33f968466e10159156feafa6445e4ef7
s4iTen <ychouk102@gmail.com>
2022-11-07 12:40:11 +0100

    header file

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,7 +2,9 @@
 #define HEADERFILE_H_
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
