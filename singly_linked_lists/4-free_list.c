f1666a2f8e324241238b482e9946d00b8bedc30c
s4iTen <ychouk102@gmail.com>
2022-11-15 16:27:40 +0100

    frees all the list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_list - this function frees all the list
+ * @head: this is the list
+ */
+void free_list(list_t *head)
+{
+	list_t *f;
+
+	while (head != NULL)
+	{
+		f = head;
+		head = head->next;
+		free(f);
+	}
+}
