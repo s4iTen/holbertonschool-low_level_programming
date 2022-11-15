5a062e9aa35b8cdadfd8a795d1d29344947b9e27
s4iTen <ychouk102@gmail.com>
2022-11-15 15:57:22 +0100

    add new node at the beginning

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,12 +10,17 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 
 	new_node = (list_t *)malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	if (str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
-	if (new_node == NULL)
-		return (new_node);
-	else
-		return (NULL);
+
+	return (new_node);
 }
