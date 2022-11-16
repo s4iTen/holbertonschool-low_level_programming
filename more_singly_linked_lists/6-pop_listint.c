6b04beeba91059f0f6368249e43a20e6e1f07225
s4iTen <ychouk102@gmail.com>
2022-11-16 19:15:14 +0100

    delete the first node in the list

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -1,14 +1,15 @@
 #include "lists.h"
-
+/**
+ * pop_listint - this is a function that delete the first node in the list
+ * @head: this is the list
+ * Return: Always '0'
+ */
 int pop_listint(listint_t **head)
 {
-    listint_t *d;
-    
-    while (*head != NULL)
-    {
-        d = *head;
-        *head = (*head)->next;
-        free(d);
-    }
-    return (0);
+	listint_t *d;
+
+	d = *head;
+	*head = (*head)->next;
+	free(d);
+	return (0);
 }
