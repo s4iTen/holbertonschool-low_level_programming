8f9de0d132bfc5d4d9edf16fac58bac0185accc8
s4iTen <ychouk102@gmail.com>
2022-11-17 21:03:08 +0100

    function that return the sum of all the data (n) in the node

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * function that return the sum of all the data in the node
+ * @head: this is the node
+ * Return: Always the sum of the data (n) of the node
+ */
+int sum_listint(listint_t *head)
+{
+	int i = 0;
+	while (head != NULL)
+	{
+		i += head->n;
+		head = head->next;
+	}
+	return (i);
+}
