c62c7c6477ac0e988cb7fc1fde53fd48a456d454
s4iTen <ychouk102@gmail.com>
2022-11-26 15:39:11 +0100

    this function add new element in hash tablde

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+    hash_node_t *new;
+    char *cp_value;
+    unsigned int index, i;
+
+    if (ht == NULL || key == NULL || value == NULL)
+        return (0);
+    cp_value = strdup(value);
+    if (cp_value == NULL)
+        return (0);
+    index = key_index((const unsigned char *)key, ht->size);
+    i = index;
+    while (ht->array[i])
+    {
+        if (strcmp(ht->array[i]->key, key) == 0)
+        {
+            ht->array[i]->value = cp_value;
+            return (1);
+        }
+        
+    }
+    new = malloc(sizeof(hash_node_t));
+    if (new == NULL)
+    {
+        free (cp_value);
+        return (0);
+    }
+    new->key = strdup(key);
+    if (new->key == NULL)
+    {
+        free(new);
+        return (0);
+    }
+    new->value = cp_value;
+    new->next = ht->array[index];
+    ht->array[index] = new;
+    return (1);
+}
