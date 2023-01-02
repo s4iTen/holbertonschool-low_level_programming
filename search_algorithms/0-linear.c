08ebbaf696b8de979af4823277aa8d8a96e5015f
s4iTen <ychouk102@gmail.com>
2023-01-02 13:54:29 +0100

    this function is a searching function and it is based on linear search

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+/**
+ * linear_search - this function is a searching function based on linear search
+ * @array: this is the array that we have to search in
+ * @size: this is the size of the array that we have
+ * @value: this is the value that we are searching for
+ * Return: always the index of the value or -1 if the value is not in the array
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+	int j = 0;
+
+	while (i < size - 1 && array)
+	{
+		printf("Value checked array[%d] = [%d]\n", j, array[j]);
+		if (array[j] == value)
+		{
+			return (j);
+		}
+		i++;
+		j++;
+	}
+	return (-1);
+}
