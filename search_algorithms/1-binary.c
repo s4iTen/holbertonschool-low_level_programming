eb4ac35a6d221b8e0d8c5625d8fbf16f2d87028b
s4iTen <ychouk102@gmail.com>
2023-01-03 12:47:17 +0100

    this function search the value in the array and it based on the binary search

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,5 +1,10 @@
 #include "search_algos.h"
-
+/**
+ * print_array - this function print the array
+ * @array: this is the array that have to be printed
+ * @high: this is the upper bound
+ * @low: this is the lower bound
+ */
 void print_array(int *array, size_t high, size_t low)
 {
 	if (low > high)
@@ -20,26 +25,47 @@ void print_array(int *array, size_t high, size_t low)
 	}
 	printf("\n");
 }
-
+/**
+ * binary_search_recursive - this func search the value and it based on
+ * Binary search and it works with recursion
+ * @array: this is the array
+ * @value: this is the value that we are looking for
+ * @low: this is the lower bound of array
+ * @high: this is the higher bound of array
+ * Return: the position of the value or (-1) if it fail
+ */
 int binary_search_recursive(int *array, int value, size_t low, size_t high)
 {
-    int mid = low + (high - low) / 2;
+	int mid = low + (high - low) / 2;
 
-    if (low > high) {
-        return -1;
-    }
-    print_array(array, high, low);
-    if (array[mid] == value) {
-        return mid;
-    } else if (array[mid] < value) {
-        return binary_search_recursive(array, value, mid + 1, high);
-    } else {
-        return binary_search_recursive(array, value, low, mid - 1);
-    }
+	if (low > high)
+	{
+		return (-1);
+	}
+	print_array(array, high, low);
+	if (array[mid] == value)
+	{
+		return (mid);
+	}
+	else if (array[mid] < value)
+	{
+		return (binary_search_recursive(array, value, mid + 1, high));
+	}
+	else
+	{
+		return (binary_search_recursive(array, value, low, mid - 1));
+	}
 }
+/**
+ * binary_search - this function calls the binary search func
+ * @array: this is the array
+ * @size: this is the size of the array
+ * @value: this is the value that we are searching for
+ * Return: always the position or (-1) if the array doesnt exist
+ */
 int binary_search(int *array, size_t size, int value)
 {
-    if (array == NULL)
-        return (-1);
-    return binary_search_recursive(array, value, 0, size - 1);
+	if (array == NULL)
+		return (-1);
+	return (binary_search_recursive(array, value, 0, size - 1));
 }
